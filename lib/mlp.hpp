e5a7bb0a6176a8e9932a6353f583709b3fc2bb89
junyoung-sim <jysim.ny@gmail.com>
2024-08-04 14:27:24 -0400

    2024-08-04

--- lib/mlp.hpp
+++ lib/mlp.hpp
@@ -4,36 +4,22 @@
 #include <vector>
 #include <random>
 
-typedef struct std::vector<float> vec1d;
-typedef struct std::vector<vec1d> vec2d;
-typedef struct std::vector<vec2d> vec3d;
-
-#define MAX_LAYERS 100
-#define MAX_NODES 1000
-
 float relu(float x);
 float drelu(float x);
 
 class MLP
 {
 private:
-    vec2d _bias;
-    vec2d _sum;
-    vec2d _act;
-    vec2d _err;
-    vec3d _weight;
     unsigned int _input_size;
-    unsigned int _num_of_layers;
-    unsigned int _shape[MAX_LAYERS];
+    std::vector<unsigned int> _shape;
+    std::vector<std::vector<float>> _bias;
+    std::vector<std::vector<float>> _sum;
+    std::vector<std::vector<float>> _act;
+    std::vector<std::vector<float>> _err;
+    std::vector<std::vector<std::vector<float>>> _weight;
 public:
     MLP() {
-        _bias.resize(MAX_LAYERS, vec1d(MAX_NODES));
-        _sum.resize(MAX_LAYERS, vec1d(MAX_NODES));
-        _act.resize(MAX_LAYERS, vec1d(MAX_NODES));
-        _err.resize(MAX_LAYERS, vec1d(MAX_NODES));
-        _weight.resize(MAX_LAYERS, vec2d(MAX_NODES, vec1d(MAX_NODES)));
-        
-        _input_size = _num_of_layers = 0;
+        _input_size = 0;
     }
     ~MLP() {}
 
--- src/mlp.cpp
+++ src/mlp.cpp
@@ -14,20 +14,27 @@ void MLP::set_input_size(unsigned int size) {
 }
 
 void MLP::add_layer(unsigned int size) {
-    if(_num_of_layers < MAX_LAYERS) {
-        _shape[_num_of_layers] = size;
-        _num_of_layers++;
-    }
+    _shape.push_back(size);
+    _bias.push_back(std::vector<float>(size, 0.0f));
+    _sum.push_back(std::vector<float>(size, 0.0f));
+    _act.push_back(std::vector<float>(size, 0.0f));
+    _err.push_back(std::vector<float>(size, 0.0f));
+    _weight.push_back(std::vector<std::vector<float>>(size, std::vector<float>()));
 }
 
 void MLP::initialize(std::default_random_engine &seed) {
     std::normal_distribution<float> gaussian(0.0f, 1.0f);
-    for(unsigned int l = 0; l < _num_of_layers; l++) {
+    for(unsigned int l = 0; l < _shape.size(); l++) {
         for(unsigned int n = 0; n < _shape[l]; n++) {
             unsigned int in_features = (l == 0 ? _input_size : _shape[l-1]);
-            for(unsigned int i = 0; i < in_features; i++)
-                _weight[l][n][i] = gaussian(seed) / in_features;
-            _bias[l][n] = _sum[l][n] = _act[l][n] = _err[l][n] = 0.0f;
+            for(unsigned int i = 0; i < in_features; i++) {
+                _weight[l][n].push_back(gaussian(seed) / in_features);
+                std::cout << _weight[l][n][i] << " ";
+            }
+            std::cout << _bias[l][n] << " ";
+            std::cout << _sum[l][n] << " ";
+            std::cout << _act[l][n] << " ";
+            std::cout << _err[l][n] << "\n";
         }
     }
 }
