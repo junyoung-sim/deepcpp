7d896af3cb3100a3191e153de4145c938b3b01cf
junyoung-sim <jysim.ny@gmail.com>
2024-08-06 12:45:51 -0400

    2024-08-06

--- lib/cnn.hpp
+++ lib/cnn.hpp
@@ -4,33 +4,6 @@
 #include <vector>
 #include "mlp.hpp"
 
-class CNN
-{
-private:
-    unsigned int _input_rows;
-    unsigned int _input_cols;
-    unsigned int _num_of_layers;
-    std::vector<std::vector<float>> _kernel;
-    std::vector<std::vector<unsigned int>> _kernel_shape;
-    std::vector<std::vector<float>> _pool;
-    std::vector<std::vector<unsigned int>> _pool_shape;
-    MLP _mlp;
-public:
-    CNN() {
-        _input_rows = _input_cols = _num_of_layers = 0;
-    }
-    ~CNN() {}
 
-    unsigned int input_rows();
-    unsigned int input_cols();
-    unsigned int num_of_layers();
-    std::vector<std::vector<float>> *kernel();
-    std::vector<std::vector<unsigned int>> *kernel_shape();
-    std::vector<std::vector<float>> *pool();
-    std::vector<std::vector<unsigned int>> *pool_shape();
-    MLP *mlp();
-
-    
-};
 
 #endif
--- src/cnn.cpp
+++ src/cnn.cpp
@@ -2,12 +2,3 @@
 #include <cstdlib>
 #include "../lib/cnn.hpp"
 
-unsigned int CNN::input_rows() { return _input_rows; }
-unsigned int CNN::input_cols() { return _input_cols; }
-unsigned int CNN::num_of_layers() { return _num_of_layers; }
-std::vector<std::vector<float>> *CNN::kernel() { return &_kernel; }
-std::vector<std::vector<unsigned int>> *CNN::kernel_shape() { return &_kernel_shape; }
-std::vector<std::vector<float>> *CNN::pool() { return &_pool; }
-std::vector<std::vector<unsigned int>> *CNN::pool_shape() { return &_pool_shape; }
-MLP *CNN::mlp() { return &_mlp; }
-
