f64f58a03695bdb4146ece3222a7f44bec371d07
junyoung-sim <jysim.ny@gmail.com>
2024-08-06 22:31:41 -0400

    2024-08-06

--- src/cnn.cpp
+++ src/cnn.cpp
@@ -8,7 +8,7 @@ void ConvPool2D::use_pooling(unsigned int pool_rows, unsigned int pool_cols) {
 }
 
 std::vector<std::vector<float>> ConvPool2D::forward(std::vector<std::vector<float>> &x) {
-    std::vector<std::vector<float>> out;
+    std::vector<std::vector<float>> conv;
     for(unsigned int i0 = 0; i0 <= x.size() - _kernel_rows; i0++) {
         std::vector<float> row;
         for(unsigned int j0 = 0; j0 <= x[i0].size() - _kernel_cols; j0++) {
@@ -19,7 +19,23 @@ std::vector<std::vector<float>> ConvPool2D::forward(std::vector<std::vector<floa
             }
             row.push_back(relu(sum));
         }
-        out.push_back(row);
+        conv.push_back(row);
     }
-    return out;
+
+    if(_pool_rows == 0 && _pool_cols == 0) return conv;
+
+    std::vector<std::vector<float>> pool;
+    for(unsigned int i0 = 0; i0 <= conv.size() - _pool_rows; i0 += _pool_rows) {
+        std::vector<float> row;
+        for(unsigned int j0 = 0; j0 <= conv[0].size() - _pool_cols; j0 += _pool_cols) {
+            float max_value = conv[i0][j0];
+            for(unsigned int i = i0; i < i0 + _pool_rows; i++) {
+                for(unsigned int j = j0; j < j0 + _pool_cols; j++)
+                    max_value = std::max(conv[i][j], max_value);
+            }
+            row.push_back(max_value);
+        }
+        pool.push_back(row);
+    }
+    return pool;
 }
