f79459e2c1cea1e646e40521281f9bd97fa45aa4
junyoung-sim <jysim.ny@gmail.com>
2024-08-18 01:25:23 -0400

    2024-08-18

--- lib/mlp.hpp
+++ lib/mlp.hpp
@@ -11,26 +11,33 @@ class MLP
 {
 private:
     unsigned int _input_size;
+    unsigned int _backward_count;
     std::vector<unsigned int> _shape;
-    std::vector<std::vector<float>> _bias;
     std::vector<std::vector<float>> _sum;
     std::vector<std::vector<float>> _act;
     std::vector<std::vector<float>> _err;
+    std::vector<std::vector<float>> _bias;
+    std::vector<std::vector<float>> _bias_grad;
     std::vector<std::vector<std::vector<float>>> _weight;
+    std::vector<std::vector<std::vector<float>>> _weight_grad;
     std::string _output_type;
+
 public:
     MLP() {
         _input_size = 0;
+        _backward_count = 0;
     }
     ~MLP() {}
 
     unsigned int input_size();
     std::vector<unsigned int> *shape();
-    std::vector<std::vector<float>> *bias();
     std::vector<std::vector<float>> *sum();
     std::vector<std::vector<float>> *act();
     std::vector<std::vector<float>> *err();
+    std::vector<std::vector<float>> *bias();
+    std::vector<std::vector<float>> *bias_grad();
     std::vector<std::vector<std::vector<float>>> *weight();
+    std::vector<std::vector<std::vector<float>>> *weight_grad();
     std::string output_type();
 
     void set_input_size(unsigned int size);
@@ -40,7 +47,9 @@ public:
 
     std::vector<float> forward(std::vector<float> &x);
 
-    std::vector<float> update(std::vector<float> &x, std::vector<float> &y, float alpha, float lambda);
+    void backward(std::vector<float> &x, std::vector<float> &y, float alpha, float lambda);
+    void step();
+    void zero_grad();
 };
 
 #endif
--- src/mlp.cpp
+++ src/mlp.cpp
@@ -7,11 +7,13 @@
 
 unsigned int MLP::input_size() { return _input_size; }
 std::vector<unsigned int> *MLP::shape() { return &_shape; }
-std::vector<std::vector<float>> *MLP::bias() { return &_bias; }
 std::vector<std::vector<float>> *MLP::sum() { return &_sum; }
 std::vector<std::vector<float>> *MLP::act() { return &_act; }
 std::vector<std::vector<float>> *MLP::err() { return &_err; }
+std::vector<std::vector<float>> *MLP::bias() { return &_bias; }
+std::vector<std::vector<float>> *MLP::bias_grad() { return &_bias_grad; }
 std::vector<std::vector<std::vector<float>>> *MLP::weight() { return &_weight; }
+std::vector<std::vector<std::vector<float>>> *MLP::weight_grad() { return &_weight_grad; }
 std::string MLP::output_type() { return _output_type; }
 
 void MLP::set_input_size(unsigned int size) { _input_size = size; }
@@ -20,11 +22,13 @@ void MLP::set_output_type(std::string output_type) { _output_type = output_type;
 
 void MLP::add_layer(unsigned int size) {
     _shape.push_back(size);
-    _bias.push_back(std::vector<float>(size, 0.0f));
     _sum.push_back(std::vector<float>(size, 0.0f));
     _act.push_back(std::vector<float>(size, 0.0f));
     _err.push_back(std::vector<float>(size, 0.0f));
+    _bias.push_back(std::vector<float>(size, 0.0f));
+    _bias_grad.push_back(std::vector<float>(size, 0.0f));
     _weight.push_back(std::vector<std::vector<float>>(size, std::vector<float>()));
+    _weight_grad.push_back(std::vector<std::vector<float>>(size, std::vector<float>()));
 }
 
 void MLP::initialize(std::default_random_engine &seed) {
@@ -32,8 +36,10 @@ void MLP::initialize(std::default_random_engine &seed) {
     for(unsigned int l = 0; l < _shape.size(); l++) {
         unsigned int in_features = (l == 0 ? _input_size : _shape[l-1]);
         for(unsigned int n = 0; n < _shape[l]; n++) {
-            for(unsigned int i = 0; i < in_features; i++)
+            for(unsigned int i = 0; i < in_features; i++) {
                 _weight[l][n].push_back(gaussian(seed) / in_features);
+                _weight_grad[l][n].push_back(0.0f);
+            }
         }
     }
 }
@@ -60,9 +66,9 @@ std::vector<float> MLP::forward(std::vector<float> &x) {
     return _act.back();
 }
 
-std::vector<float> MLP::update(std::vector<float> &x, std::vector<float> &y, float alpha, float lambda) {
+void MLP::backward(std::vector<float> &x, std::vector<float> &y, float alpha, float lambda) {
     std::vector<float> out = forward(x);
-    std::vector<float> ierr(_input_size, 0.0f);
+    //std::vector<float> ierr(_input_size, 0.0f);
     for(int l = _shape.size() - 1; l >= 0; l--) {
         float partial_gradient = 0.0f, full_gradient = 0.0f;
         unsigned int in_features = (l == 0 ? _input_size : _shape[l-1]);
@@ -73,20 +79,44 @@ std::vector<float> MLP::update(std::vector<float> &x, std::vector<float> &y, flo
             }
             else partial_gradient = _err[l][n] * drelu(_sum[l][n]);
 
-            _bias[l][n] -= alpha * partial_gradient;
+            _bias_grad[l][n] += alpha * partial_gradient;
             for(unsigned int i = 0; i < in_features; i++) {
                 if(l == 0) {
                     full_gradient = partial_gradient * x[i];
-                    ierr[i] += partial_gradient * _weight[l][n][i];
+                    //ierr[i] += partial_gradient * _weight[l][n][i];
                 }
                 else {
                     full_gradient = partial_gradient * _act[l-1][i];
                     _err[l-1][i] += partial_gradient * _weight[l][n][i];
                 }
                 full_gradient += lambda * _weight[l][n][i];
-                _weight[l][n][i] -= alpha * full_gradient;
+                _weight_grad[l][n][i] += alpha * full_gradient;
             }
         }
     }
-    return ierr;
+    _backward_count++;
+    //return ierr;
+}
+
+void MLP::step() {
+    for(unsigned int l = 0; l < _shape.size(); l++) {
+        unsigned int in_features = (l == 0 ? _input_size : _shape[l-1]);
+        for(unsigned int n = 0; n < _shape[l]; n++) {
+            _bias[l][n] -= _bias_grad[l][n] / _backward_count;
+            for(unsigned int i = 0; i < in_features; i++)
+                _weight[l][n][i] -= _weight_grad[l][n][i] / _backward_count;
+        }
+    }
+}
+
+void MLP::zero_grad() {
+    for(unsigned int l = 0; l < _shape.size(); l++) {
+        unsigned int in_features = (l == 0 ? _input_size : _shape[l-1]);
+        for(unsigned int n = 0; n < _shape[l]; n++) {
+            _bias_grad[l][n] = 0.0f;
+            for(unsigned int i = 0; i < in_features; i++)
+                _weight_grad[l][n][i] = 0.0f;
+        }
+    }
+    _backward_count = 0;
 }
